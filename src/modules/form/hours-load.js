bc0fd8a843470ec2be3a397871f286288979ef00
AndreTrevizam <andretrevizam264@gmail.com>
2025-01-24 17:16:48 -0300

    selecionando o horário disponivel

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,4 +1,5 @@
 import { openingHours } from "../../utils/opening-hours.js"
+import { hoursClick } from "./hours-click.js"
 import dayjs from "dayjs"
 
 const hours = document.getElementById("hours")
@@ -25,6 +26,26 @@ export function hoursLoad({date}) {
     li.classList.add(available ? "hour-available" : "hour-unavailable")
 
     li.textContent = hour
+
+    if (hour === "9:00") {
+      hourHeaderAdd("Manhã")
+    } else if (hour === "13:00") {
+      hourHeaderAdd("Tarde")
+    } else if (hour === "18:00") {
+      hourHeaderAdd("Noite")
+    }
+
     hours.append(li)
   })
+
+  // Adiciona o evento de clique nos horarios disponiveis
+  hoursClick()
+}
+
+function hourHeaderAdd(title) {
+  const header = document.createElement("li")
+  header.classList.add("hour-period")
+  header.textContent = title
+
+  hours.append(header)
 }
