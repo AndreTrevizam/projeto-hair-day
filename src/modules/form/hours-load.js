973354e943cbe2e690aec044791ef55c906ebd68
AndreTrevizam <andretrevizam264@gmail.com>
2025-01-25 19:02:09 -0300

    Bloquenado horários já agendados

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -5,20 +5,25 @@ import dayjs from "dayjs"
 const hours = document.getElementById("hours")
 
 
-export function hoursLoad({date}) {
+export function hoursLoad({date, dailySchedules}) {
   // Limpa a lista de horarios
   hours.innerHTML = ""
+
+  // Obtém a lista de horários ocupados
+  const unavailableHours = dailySchedules.map((schedule) => dayjs(schedule.when).format("HH:mm"))
   
   const opening = openingHours.map((hour) => {
     // Recupera somente a hora
     const [scheduleHour] = hour.split(":")
 
     // Adiciona a hora na data e verificar se esta no passado
-    const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
+    const isHourPast = dayjs(date).add(scheduleHour, "hour").isBefore(dayjs())
     
+    const available = !unavailableHours.includes(hour) && !isHourPast
+
     return {
       hour,
-      available: isHourPast
+      available,
     }
   })
 
