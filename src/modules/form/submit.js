870e05dff14b7beaa7256251d4fd54b33b5ad88d
AndreTrevizam <andretrevizam264@gmail.com>
2025-01-25 19:11:39 -0300

    Atualizando a lista de agendamentos

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,5 +1,6 @@
 import dayjs from "dayjs"
 import {scheduleNew} from "../../services/schedule-new.js"
+import {schedulesDay} from "../schedules/load.js"
 
 const form = document.querySelector("form")
 const clientName = document.getElementById("client")
@@ -41,11 +42,18 @@ form.onsubmit = async (event) => {
     // Gera um ID
     const id = new Date().getTime()
 
+    // Faz o agendamento
     await scheduleNew({
       id,
       name,
       when
     })
+
+    // Recarregar os agendamentos
+    await schedulesDay()
+
+    // Limpa o input de nome do cliente
+    clientName.value = ""
 
   } catch (error) {
     console.log(error)
